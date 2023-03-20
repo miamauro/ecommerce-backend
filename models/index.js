c13839f3d3cbeaa294315c493b4af6d5a57de177
miamauro <miiamauro@outlook.com>
2023-03-20 18:34:33 -0500

    edited association methods

--- models/index.js
+++ models/index.js
@@ -15,10 +15,20 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: "ProductTag" });
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: "ProductTag" });
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
